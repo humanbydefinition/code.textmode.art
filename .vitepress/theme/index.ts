cc73bc2cbd0c14f09e00dd8c51321b87bed19333
humanbydefinition <hello@textmode.art>
2026-04-23 20:11:15 +0200

    feat: add textmode.figlet.js script to CodeSandbox configuration

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -101,7 +101,7 @@ const baseTheme: Theme = {
     codeblocksFold({ route, frontmatter })
     useCodeSandbox({
       languages: ['javascript', 'typescript'],
-      scripts: ['https://unpkg.com/textmode.js@latest/dist/textmode.umd.js', 'https://unpkg.com/textmode.synth.js@latest/dist/textmode.synth.umd.js', 'https://unpkg.com/textmode.filters.js@latest/dist/textmode.filters.umd.js'],
+      scripts: ['https://unpkg.com/textmode.js@latest/dist/textmode.umd.js', 'https://unpkg.com/textmode.synth.js@latest/dist/textmode.synth.umd.js', 'https://unpkg.com/textmode.filters.js@latest/dist/textmode.filters.umd.js', 'https://unpkg.com/textmode.figlet.js@latest/dist/textmode.figlet.umd.js'],
       filename: 'sketch.js',
       buttonText: 'Open in CodeSandbox',
       indexHtml: `<!DOCTYPE html>
